78a25cec1a29d238ec9e572862670f16c6fd6bde
Yusuf Umer <yusufumer26@gmail.com>
2026-08-06 13:27:28 +0300

    feat(enrollment-form): implement reactive form model and validation

--- src/app/features/enrollment-form/enrollment-form.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-enrollment-form',
-  imports: [],
-  templateUrl: './enrollment-form.html',
-  styleUrl: './enrollment-form.scss',
-})
-export class EnrollmentForm {}
