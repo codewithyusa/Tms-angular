2990e411f27eab5acb1d864dfc1d6d2b7a9f5c60
Yusuf Umer <yusufumer26@gmail.com>
2026-08-13 21:21:43 +0300

    refactor: update CourseService injection and API config

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -2,16 +2,20 @@ import { Injectable, inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { Course, CourseDetail, PagedResponse } from "../models/course.model";
+import { environment } from "../../environments/environment";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class CourseService {
   private http = inject(HttpClient);
-  private baseUrl = "http://localhost:5249/api/courses";
+  private readonly baseUrl = `${environment.apiUrl}/courses`;
 
   getAll() {
     return this.http
       .get<PagedResponse<Course>>(this.baseUrl, {
-        params: { page: "1", pageSize: "50" },
+        params: {
+          page: "1",
+          pageSize: "50",
+        },
       })
       .pipe(map((p) => p.items));
   }
