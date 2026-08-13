89e9163f53d361bb7638ba75c9b7f6de34c5e0ae
Yusuf Umer <yusufumer26@gmail.com>
2026-08-13 21:46:18 +0300

    feat: configure credentials interceptor and XSRF

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,34 @@
 import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
+import {
+  provideHttpClient,
+  withInterceptors,
+  withXsrfConfiguration,
+} from "@angular/common/http";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+
 import { routes } from "./app.routes";
+import { credentialsInterceptor } from "./interceptors/credentials.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(),
+
+    provideRouter(
+      routes,
+      withComponentInputBinding()
+    ),
+
+    provideHttpClient(
+      withInterceptors([
+        credentialsInterceptor,
+      ]),
+      withXsrfConfiguration({
+        cookieName: "XSRF-TOKEN",
+        headerName: "X-XSRF-TOKEN",
+      })
+    ),
+
     provideAnimationsAsync(),
   ],
 };
