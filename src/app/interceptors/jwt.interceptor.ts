c9057119e1a17fbc09603e85e4846904c2198855
Yusuf Umer <yusufumer26@gmail.com>
2026-08-23 21:31:19 +0300

    feat(auth): add JWT HTTP interceptor

---
+++ src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,27 @@
+import { inject } from "@angular/core";
+import {
+  HttpInterceptorFn
+} from "@angular/common/http";
+
+import { AuthService } from "../services/auth.service";
+
+export const jwtInterceptor: HttpInterceptorFn = (
+  req,
+  next
+) => {
+  const auth = inject(AuthService);
+
+  const token = auth.getAccessToken();
+
+  if (!token) {
+    return next(req);
+  }
+
+  const clonedRequest = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return next(clonedRequest);
+};
