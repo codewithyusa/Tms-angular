8091779efa2f833275101504ee1a3caa9b80dea0
Yusuf Umer <yusufumer26@gmail.com>
2026-09-02 19:40:44 +0300

    feat: add create method to EnrollmentService

--- src/app/services/enrollment.service.ts
+++ src/app/services/enrollment.service.ts
@@ -3,16 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Enrollment } from '../models/enrollment.model';
 
+export interface EnrollStudentRequest {
+  studentId: number;
+}
+
 @Injectable({ providedIn: 'root' })
 export class EnrollmentService {
   private http = inject(HttpClient);
-  private baseUrl = 'http://localhost:5249/api/enrollments';
+  private baseUrl = 'http://localhost:5249/api';
 
   getAll(): Observable<Enrollment[]> {
-    return this.http.get<Enrollment[]>(this.baseUrl);
+    return this.http.get<Enrollment[]>(`${this.baseUrl}/enrollments`);
   }
 
-  approve(id: string): Observable<Enrollment> {
-    return this.http.post<Enrollment>(`${this.baseUrl}/${id}/approve`, {});
+  create(courseId: number, body: EnrollStudentRequest): Observable<Enrollment> {
+    return this.http.post<Enrollment>(
+      `${this.baseUrl}/courses/${courseId}/enrollments`,
+      body
+    );
+  }
+
+  approve(id: string): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/enrollments/${id}/approve`, {});
   }
 }
